38222d6dd7b143dcc5de6254861739a26f015fac
Andre Oliveira <andreluisoliveira013@gmail.com>
2022-08-08 09:16:01 -0300

    fix: adjustments cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./src/routes/index');
 var usersRouter = require('./src/routes/users');
@@ -19,14 +20,35 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+const allowedOrigins = [
+  'http://localhost',
+  'http://localhost:8080',
+  'http://localhost:3000',
+  'https://solvee.app.br'
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Origin not allowed by CORS'));
+    }
+  },
+};
+
+app.options('*', cors(corsOptions));
+app.use('/', cors(corsOptions), indexRouter);
+app.use('/users', cors(corsOptions), usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
+
 
 bot.client;
 
